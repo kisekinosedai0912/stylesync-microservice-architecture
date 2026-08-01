7dc0b62dba11718e958b7e6ff75a65cd6d93342d
jasper-uplow <jasper@uplow.com>
2026-08-01 18:06:05 +0800

    about page is done

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,11 +2,13 @@ import { lazy } from "react";
 import Layout from "./layout";
 
 const Hero = lazy(() => import("./components/Hero"));
+const About = lazy(() => import("./components/About"));
 
 export function App() {
     return (
         <Layout>
             <Hero />
+            <About />
         </Layout>
     );
 }
