c745541930e65f54f823719ff7e0c4696c1c3224
jasper-uplow <jasper@uplow.com>
2026-08-05 11:03:29 +0800

    Ignore generated dist folders for shared libs and sentry

--- libs/sentry/dist/handler.d.ts
+++
@@ -1,9 +0,0 @@
-import type { LogContext } from "@stylesync/types";
-declare class ErrorHandler {
-    normalizeError(error: unknown, fallbackMessage: string): Error;
-    serializeError(error: unknown): unknown;
-    sanitizeContext(context: LogContext): LogContext;
-}
-export declare const sentryHandler: ErrorHandler;
-export {};
-//# sourceMappingURL=handler.d.ts.map
--- libs/sentry/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { createLogger, SentryLogger } from "./logger";
-export { sentryHandler } from "./handler";
-//# sourceMappingURL=index.d.ts.map
