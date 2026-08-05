504626400ce89b21cdd2b4b1448897dcedf0d230
jasper-uplow <jasper@uplow.com>
2026-08-05 12:59:02 +0800

    another cleanup commit

--- libs/auth/dist/AuthField.d.ts
+++
@@ -1,7 +0,0 @@
-import type { InputHTMLAttributes } from "react";
-export interface AuthFieldProps extends Omit<InputHTMLAttributes<HTMLInputElement>, "className"> {
-    label: string;
-    hint?: string;
-}
-export declare function AuthField({ label, hint, id, name, ...props }: AuthFieldProps): import("react/jsx-runtime").JSX.Element;
-//# sourceMappingURL=AuthField.d.ts.map
